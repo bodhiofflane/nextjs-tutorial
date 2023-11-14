f4e5a60ea0c1e0f3ffcef9cabc5a7bd426f39a79
bodhiofflane <channelbodhi@gmail.com>
2023-11-14 13:10:10 +0300

    Image

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { getAllPosts } from '@/services/getPosts';
 import { Metadata } from "next";
 
+import Image from 'next/image';
+
+// Здесь не ссылка на ресурс как в React, а обхект со свойствами: src, ширина, высота и прочее.
+import img1 from '../../../assets/1.jpg';
+import img2 from '../../../assets/2.jpg';
+
 async function getData(id: string) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}`,
@@ -42,10 +48,26 @@ const Post = async ({ params }: Props) => {
   const post = await getData(params.id);
 
   return (
-    <>
+    <div style={{display: 'flex', flexDirection: 'column' , alignItems: 'center', justifyContent: 'center'}}>
       <h2>{post.title}</h2>
+      <div style={{width: '500px'}}>
+      <Image
+        style={{width: '100%', height: 'auto'}}
+        className={`sd`}
+        //initial={``}
+        //animate={``}
+        alt={`Woman`} // Обязательный проп.
+        // Автоматически передается и ширина с высотой.
+        src={img1.src} // Обязательный проп. Ширина и высота тоже обязательные.
+        // Если указываем в src строку с адресом, то ширину и высоту нужно указывать вручную. Это для того что бы браузер резервовал место под img.
+        width={500} 
+        height={1000}
+        priority // Изображение в видемой облости загрузится в с самым высоким приоритетом.
+      />
+      </div>
+
       <p>{post.body}</p>
-    </>
+    </div>
   );
 };
 
--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [375, 640, 750, 828, 1080, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/vi/**'
+      },
+    ]
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  swcMinify: true,
+}
 
 module.exports = nextConfig
 
